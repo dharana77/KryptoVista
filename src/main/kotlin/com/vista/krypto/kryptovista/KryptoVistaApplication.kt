85753c80644ef8bbf82dd72ce41f310b29a00093
dharana lee <dharana77@studiolab.ai>
2024-01-02 15:12:24 +0900

    chore: project init

---
+++ src/main/kotlin/com/vista/krypto/kryptovista/KryptoVistaApplication.kt
@@ -0,0 +1,11 @@
+package com.vista.krypto.kryptovista
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KryptoVistaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KryptoVistaApplication>(*args)
+}
---
+++ src/test/kotlin/com/vista/krypto/kryptovista/KryptoVistaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vista.krypto.kryptovista
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KryptoVistaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
